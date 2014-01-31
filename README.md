38d7e42d5b8b725d9bfa04b854465987dceb111c
gaer <n.gaer@parcsis.org>
2014-02-01 01:59:24 +0400

    Initial

---
+++ README.md
@@ -0,0 +1,76 @@
+# gulp-node-spritesheet [![Build Status](https://secure.travis-ci.org/ngaer/gulp-node-spritesheet.png?branch=master)](http://travis-ci.org/ngaer/gulp-node-spritesheet)
+
+Gulp plugin for [node-spritesheet utility](https://github.com/richardbutler/node-spritesheet)
+
+## Requirements
+node-spritesheet utility requires [ImageMagick](http://www.imagemagick.org/script/index.php), available via HomeBrew (`$ sudo brew install ImageMagick`) or MacPorts: (`$ sudo port install ImageMagick`).
+
+## Installation
+Install the module with: `npm install gulp-node-spritesheet`
+
+## Usage
+    var sprite = require('gulp-node-spritesheet');
+
+    gulp.task('sprite', function() {
+        gulp.src('icons/*.png')
+        	.pipe(sprite({
+                // Path for compiled sprite styles
+    			outputCss: 'sprite/style.css',
+
+                // Uses this compound selector in the css, e.g. '.sprite.my-image {}'
+    			selector: '.sprite',
+
+                // Name for sprite image file
+    			outputImage: 'image.png'
+    		}))
+    		.pipe(gulp.dest('sprite'));
+    });
+
+Takes in a series of images a generates a sprite styles in `sprite/style.css` and passes sprite image `image.png` output.
+
+### Retina example
+
+    var sprite = require('gulp-node-spritesheet');
+
+    gulp.src('images/icons/*.png')
+		.pipe(sprite({
+			outputCss: './css/sprite.css',
+			selector: '.sprite',
+
+            // Optional ImageMagick sampling filter.
+            downsampling: "LanczosSharp",
+
+            // Output configurations: in this instance to output two sprite sheets,
+            // one for "legacy" (i.e. 72dpi, pixel ratio 1), and "retina" (x2).
+            // These keys (legacy, retina) are completely arbitrary.
+			output: {
+				legacy: {
+					pixelRatio: 1,
+					outputImage: 'sprite.png',
+                    // Optional path to output image
+					httpImagePath: '../images/sprite.png'
+				},
+				retina: {
+					pixelRatio: 2,
+					outputImage: 'sprite@2x.png',
+					httpImagePath: '../images/sprite@2x.png'
+				}
+			}
+            
+            // Allows you to augment your selector names for each image, based on
+            // the bare image "name", or the full image path.
+            resolveImageSelector: function(name, fullpath) {
+                // For example, your files may well already be named with @2x, but
+                // you won't want that included in your CSS selectors.
+                return name.split('@2x').join('');
+            }
+		}))
+		.pipe(gulp.dest('images'));
+
+As the retina scheme has the highest pixel ratio, it will be assumed that all images passed to the plugin are for 'retina'. So, at the output, will be two images `sprite@2x.png` for 'retina' and downscaled `sprite.png` for 'legacy'.
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Gaer Nikita. Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,119 @@
+/*
+ * gulp-node-spritesheet
+ * https://github.com/ngaer/gulp-node-spritesheet
+ *
+ * Copyright (c) 2014 Gaer Nikita
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gutil = require('gulp-util'),
+	through = require('through'),
+	sprites = require('node-spritesheet'),
+	fs = require('fs'),
+	Q = require('q');
+
+module.exports = function(options) {
+	var builder,
+		builderOptions = {
+			images: []
+		},
+		outputOptions = options.output;
+
+	// get output directory
+	if (options.outputCss) {
+		var outputMatch = options.outputCss.match(/^(.*?)\/?([^/]*)\.([^/]*)$/);
+
+		if (outputMatch.length > 1) {
+			options.outputCss = outputMatch[2] + '.' + outputMatch[3];
+			options.outputDirectory = outputMatch[1];
+		}
+	}
+
+	// create config of allowed options
+	[
+		'selector',
+		'outputCss',
+		'outputImage',
+		'outputDirectory',
+		'downsampling',
+		'resolveImageSelector',
+		'httpImagePath',
+		'filter'
+	].forEach(function(option) {
+			if (option == 'outputImage' && outputOptions) {
+				return;
+			}
+			if (options[option]) {
+				builderOptions[option] = options[option];
+			}
+		})
+
+	function bufferFiles(file) {
+		if (file.isNull()) return;
+		file.isStream() && this.emit('error', new PluginError('gulp-node-spritesheet', 'Streams are not supported!'));
+
+		// collect image paths
+		builderOptions.images.push(file.path);
+	};
+
+	function endStream() {
+		var self = this,
+			outputFiles = [];
+
+		builder = new sprites.Builder(builderOptions);
+
+		// add output configurations
+		if (outputOptions) {
+			for (var key in outputOptions) {
+				if (outputOptions.hasOwnProperty(key)) {
+					var outputOptionsItem = outputOptions[key];
+					builder.addConfiguration(key, outputOptionsItem);
+					outputFiles.push(outputOptionsItem.outputImage);
+				}
+			}
+		} else {
+			outputFiles.push(options.outputImage);
+		}
+
+		builder.build(function() {
+			var queue = [];
+			/*
+			 Plugin node-spritesheet doesn't support streaming of compiled files, it can just save it in fs by
+			 configuration. So to make it more 'gulp way', we read this compiled files and push it to a stream, and
+			 remove files form fs after.
+			 */
+			outputFiles.forEach(function(file) {
+				var filePath = options.outputDirectory + '/' + file,
+					deferred = Q.defer();
+
+				fs.readFile(filePath, function(err, data) {
+					if (err) {
+						self.emit('error', new PluginError('gulp-node-spritesheet', err));
+					}
+					if (data) {
+						fs.unlink(filePath, function (err) {
+							if (err) throw err;
+						});
+						self.emit('data', new gutil.File({
+							cwd: options.outputDirectory,
+							base: options.outputDirectory,
+							path: filePath,
+							contents: data
+						}));
+					}
+					deferred.resolve();
+				});
+
+				queue.push(deferred.promise);
+			});
+
+			Q.all(queue).then(function() {
+				self.emit('end');
+			});
+		});
+	};
+
+	return through(bufferFiles, endStream);
+};
